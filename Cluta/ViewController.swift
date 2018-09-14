63a25b66fa780d9b7f0118bdf06e07ab3daee3e5
Ayako Nago <ayakonago@ayako-no-macbook-pro.local>
2018-09-14 17:45:53 +0900

    Initial Commit

---
+++ Cluta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cluta
+//
+//  Created by Ayako Nago on 2018/09/14.
+//  Copyright © 2018年 Ayako Nago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
